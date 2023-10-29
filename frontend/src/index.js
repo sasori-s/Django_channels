06385c70a912689de5260111726a518c5884d7ac
Sams <shams.iqbal@g.bracu.ac.bd>
2023-10-29 16:30:37 +0600

    Added htmls, css to the react and its showing the ui some errors, 3 - 13:12

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import  ReactDOM   from 'react-dom'
+import Chat from './container/chat'
 
 class App extends React.Component {
     render() {
         return (
-            <div>Hello</div>
+            <Chat />
         )
     }
 }
